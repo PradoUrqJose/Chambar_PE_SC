ac7bf53680d098dd1c12fb836e3f410567e642af
Jose Prado <josepu03@gmail.com>
2026-05-14 20:14:37 -0500

    nuevas Credenciales

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createBrowserClient } from '@supabase/ssr';
 
-const supabaseUrl = 'https://odrwnelygjcvdhhtryzr.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kcnduZWx5Z2pjdmRoaHRyeXpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyNjg3MTEsImV4cCI6MjA3NDg0NDcxMX0.VvZjq_UxnG5UA0ZpbjxCDukyPOIJ0ul0yZqCpkr0yHQ';
+const supabaseUrl = 'https://rmuhtossltxbvszmfazb.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJtdWh0b3NzbHR4YnZzem1mYXpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4MDE5NTUsImV4cCI6MjA5NDM3Nzk1NX0.wG4Dx5EMsf4EWnN5Fg6_ctzFMAN3fRb6nsihUhAdbog';
 
 export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey);
